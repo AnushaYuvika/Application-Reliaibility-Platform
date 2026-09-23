f553a411ae2f779147ac0fa2a19f78e12e2dc41e
Anusha Yuvika <yuvikaanusha19@gmail.com>
2026-09-23 17:40:37 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const cors = require("cors");
 
@@ -47,6 +47,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
